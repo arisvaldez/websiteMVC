48bc46d31d747c391fa6c68989d36bbecc200f92
aris valdez <samus45@hotmail.es>
2021-02-11 11:03:04 -0400

    Add project files.

---
+++ website/Controllers/CrudController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace website.Controllers
+{   
+    public class CrudController : Controller
+    {
+        List<Libro> libros;
+
+        public CrudController()
+        {
+            libros = new List<Libro>()
+            {
+                new Libro()
+                {
+                    Id = 1,
+                    Titulo = "La Gran Controversia",
+                    FechaPublicacion = "1880"
+                },
+
+                new Libro()
+                {
+                    Id = 2,
+                    Titulo = "La Gran Batalla",
+                    FechaPublicacion = "1910"
+                },
+                
+                new Libro()
+                {
+                    Id = 3,
+                    Titulo = "La Batalla Final",
+                    FechaPublicacion = "1980"
+                }
+            };
+        }
+
+        // GET: Crud
+        public ActionResult Index()
+        {
+            ViewBag.Datos = libros;
+            return View();
+        }
+
+        // GET: Crud/Details/5
+        public ActionResult Details(int id)
+        {
+            ViewBag.Datos = libros[id-1];
+            return View();
+        }
+
+        // GET: Crud/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Crud/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ViewBag.Datos = collection;
+                return View();
+               // return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Crud/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Crud/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Crud/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Crud/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string FechaPublicacion { get; set; }
+    }
+}
---
+++ website/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace website.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page Hola Mundo Infotep.";
+            ViewBag.Recordatorio = "Esto es para que no se te olvide";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Papaup()
+        {
+            ViewBag.Resultado = sumar(50, 501);
+            ViewBag.Nombre = "Victor Vargas Presidente";
+            
+            return View();
+        }
+
+        public double sumar(double a, double b) 
+        {
+            return a + b;
+        }
+    }
+}
